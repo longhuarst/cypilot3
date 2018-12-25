83339673becfbb636860406884e7d224c1ec9fe4
longhuarst <841105197@qq.com>
2018-12-25 23:14:47 +0800

    增加 void setDiagnosticsInformationCallback(DJIDiagnostics.DiagnosticsInformationCallback callback){}

--- app/src/main/java/cyfly/com/dji/cypilot3/MyBaseProduct.java
+++ app/src/main/java/cyfly/com/dji/cypilot3/MyBaseProduct.java
@@ -1,4 +1,28 @@
 package cyfly.com.dji.cypilot3;
 
+import dji.sdk.base.DJIDiagnostics;
+import dji.sdk.sdkmanager.DJISDKManager;
+
 public class MyBaseProduct {
+
+
+
+
+    //DJI产品的抽象类
+    //
+
+
+    //设置诊断列表更新回调函数
+    void setDiagnosticsInformationCallback(DJIDiagnostics.DiagnosticsInformationCallback callback){
+        DJISDKManager.getInstance().getProduct().setDiagnosticsInformationCallback(callback);
+    }
+
+
+
+
+
+
+
+
+
 }
