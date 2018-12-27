d1d68badc529dfd01580256b4ff9639da4e87e52
longhuarst <841105197@qq.com>
2018-12-27 23:02:44 +0800

    增加 public class MyAircraft {    }

---
+++ app/src/main/java/cyfly/com/dji/cypilot3/MyAircraft.java
@@ -0,0 +1,4 @@
+package cyfly.com.dji.cypilot3;
+
+public class MyAircraft {
+}
--- app/src/main/java/cyfly/com/dji/cypilot3/MyBaseComponent.java
+++ app/src/main/java/cyfly/com/dji/cypilot3/MyBaseComponent.java
@@ -1,6 +1,13 @@
 package cyfly.com.dji.cypilot3;
 
+import dji.sdk.base.BaseComponent;
+import dji.sdk.sdkmanager.DJISDKManager;
+
 public class MyBaseComponent {
+
+
+
+
 
 
 
