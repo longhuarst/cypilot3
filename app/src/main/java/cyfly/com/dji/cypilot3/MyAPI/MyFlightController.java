b16420d263db2a9cb714af6f32af272238ffd720
longhuarst <841105197@qq.com>
2018-12-27 23:36:09 +0800

    增加 MyFlightController 单例

--- app/src/main/java/cyfly/com/dji/cypilot3/MyAPI/MyFlightController.java
+++ app/src/main/java/cyfly/com/dji/cypilot3/MyAPI/MyFlightController.java
@@ -1,4 +1,52 @@
 package cyfly.com.dji.cypilot3.MyAPI;
 
+import android.support.annotation.NonNull;
+
+import dji.common.flightcontroller.FlightControllerState;
+import dji.sdk.base.BaseProduct;
+import dji.sdk.flightcontroller.FlightController;
+import dji.sdk.products.Aircraft;
+import dji.sdk.sdkmanager.DJISDKManager;
+
 public class MyFlightController {
+
+
+    private  MyFlightController(){
+
+    }
+
+    private static class MyFlightControllerHolder{
+        private final static MyFlightController instance = new MyFlightController();
+    }
+
+    public static MyFlightController getInstance(){
+        return MyFlightControllerHolder.instance;
+    }
+
+
+
+
+
+
+    FlightController getFlightController(){
+        BaseProduct product = DJISDKManager.getInstance().getProduct();
+
+        FlightController controller = ((Aircraft)product).getFlightController();
+
+        if (controller == null)
+            return null;
+
+        controller.setStateCallback(new FlightControllerState.Callback() {
+            @Override
+            public void onUpdate(@NonNull FlightControllerState flightControllerState) {
+
+            }
+        });
+
+        return controller;
+    }
+
+
+
+
 }
