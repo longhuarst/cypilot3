4fd751a0b5e29580430d95d36e5be250330f4a98
longhuarst <841105197@qq.com>
2018-12-30 23:15:33 +0800

    增加  任务规划

---
+++ app/src/main/java/cyfly/com/dji/cypilot3/MyMission/MyMissionControl.java
@@ -0,0 +1,270 @@
+package cyfly.com.dji.cypilot3.MyMission;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.util.List;
+
+import dji.common.error.DJIError;
+import dji.sdk.mission.MissionControl;
+import dji.sdk.mission.activetrack.ActiveTrackOperator;
+import dji.sdk.mission.followme.FollowMeMissionOperator;
+import dji.sdk.mission.hotpoint.HotpointMissionOperator;
+import dji.sdk.mission.panorama.PanoramaMissionOperator;
+import dji.sdk.mission.tapfly.TapFlyMissionOperator;
+import dji.sdk.mission.timeline.TimelineElement;
+import dji.sdk.mission.timeline.triggers.Trigger;
+import dji.sdk.mission.waypoint.WaypointMissionOperator;
+import dji.sdk.sdkmanager.DJISDKManager;
+
+public class MyMissionControl {
+
+
+    MissionControl control;
+
+
+
+    //获取任务控制
+    MissionControl getMissionControl(){
+        control = DJISDKManager.getInstance().getMissionControl();
+        return control;
+    }
+
+
+    //Mission Operators
+
+
+
+    //Returns the operator for waypoint missions.
+    @NonNull
+    WaypointMissionOperator getWaypointMissionOperator(){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return null;
+
+        return control.getWaypointMissionOperator();
+    }
+
+
+
+    //Returns the operator for Hotpoint missions.
+    @NonNull
+    HotpointMissionOperator getHotpointMissionOperator(){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return null;
+
+        return control.getHotpointMissionOperator();
+
+    }
+
+
+
+    //Returns the operator for Follow Me missions.
+    @NonNull
+    FollowMeMissionOperator getFollowMeMissionOperator(){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return null;
+
+        return control.getFollowMeMissionOperator();
+    }
+
+
+    //Returns the operator for ActiveTrack missions.
+    @NonNull
+    ActiveTrackOperator getActiveTrackOperator(){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return null;
+
+        return control.getActiveTrackOperator();
+    }
+
+
+    //Returns the operator for TapFly missions.
+    @NonNull
+    TapFlyMissionOperator getTapFlyMissionOperator(){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return null;
+
+        return control.getTapFlyMissionOperator();
+    }
+
+
+
+
+    @NonNull
+    PanoramaMissionOperator getPanoramaMissionOperator() {
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return null;
+
+        return control.getPanoramaMissionOperator();
+    }
+
+
+//========================================Timeline
+//========================================Timeline
+//========================================Timeline
+
+
+    //Adds an element to the end of the Timeline.
+    DJIError scheduleElement(@NonNull TimelineElement element){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return null;
+
+        return control.scheduleElement(element);
+    }
+
+
+
+
+    //Adds a list of elements to the end of the Timeline.
+    DJIError scheduleElements(@NonNull List<TimelineElement> elements){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return null;
+
+        return control.scheduleElements(elements);
+    }
+
+
+
+    //Insert an element into the Timeline at an index.
+    DJIError scheduleElementAtIndex(TimelineElement element, int index){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return null;
+
+
+
+        return control.scheduleElementAtIndex(element,index);
+    }
+
+
+
+    //Remove element from the Timeline. If the same element was used more than once in the Timeline, then the first one will be removed.
+    void unscheduleElement(TimelineElement element){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return;
+
+        control.unscheduleElement(element);
+    }
+
+
+    //Removes the Element at an index in the Timeline.
+    void unscheduleElementAtIndex(int index){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return;
+
+        control.unscheduleElementAtIndex(index);
+    }
+
+
+
+    //Returns the element at a given index in the Timeline.
+    @Nullable
+    TimelineElement scheduledElementAtIndex(int index){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return null;
+
+        return control.scheduledElementAtIndex(index);
+    }
+
+
+
+    //Returns the number of elements within the Timeline.
+    int scheduledCount(){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return 0;
+
+        return control.scheduledCount();
+    }
+
+
+
+    //Clears the Timeline, removing all elements.
+    void unscheduleEverything(){
+
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return;
+
+
+        control.unscheduleEverything();
+    }
+
+
+
+
+    //Set the triggers for the Timeline.
+    void setTriggers(List<Trigger> triggers){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return;
+
+        control.setTriggers(triggers);
+    }
+
+
+
+
+    //Returns the list of triggers which are dependents of the Timeline.
+    List<Trigger> getTriggers(){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return null;
+
+        return control.getTriggers();
+    }
+
+
+
+
+    //Start Timeline execution from the current element index getCurrentTimelineMarker.
+    void startTimeline(){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return ;
+
+        control.startTimeline();
+    }
+
+
+
+    //Pause execution of the Timeline, and current element (if pausible). If the current element is not pausible then this method will do nothing.
+    void pauseTimeline(){
+        if (control == null)
+            getMissionControl();
+        if (control == null)
+            return ;
+
+        control.pauseTimeline();
+    }
+
+
+}
