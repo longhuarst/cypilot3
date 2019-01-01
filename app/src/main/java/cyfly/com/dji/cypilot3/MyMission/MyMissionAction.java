eaf2349d3a7c4f60401ee6da6fe266d2c27dbd75
longhuarst <841105197@qq.com>
2019-01-01 23:36:47 +0800

    增加 getMissionAction

--- app/src/main/java/cyfly/com/dji/cypilot3/MyMission/MyMissionAction.java
+++ app/src/main/java/cyfly/com/dji/cypilot3/MyMission/MyMissionAction.java
@@ -1,14 +1,58 @@
 package cyfly.com.dji.cypilot3.MyMission;
 
+import dji.common.error.DJIError;
 import dji.sdk.mission.timeline.actions.MissionAction;
+import dji.sdk.sdkmanager.DJISDKManager;
 
 public class MyMissionAction {
 
 
 
-    MissionAction action; //任务行动
+    MissionAction action = null; //任务行动
+
+
+    MissionAction getAction(){
+        if (action != null)
+            return action;
+
+        return new MissionAction() {
+            @Override
+            protected void startListen() {
+
+            }
+
+            @Override
+            protected void stopListen() {
+
+            }
+
+            @Override
+            public void run() {
+
+            }
+
+            @Override
+            public boolean isPausable() {
+                return false;
+            }
+
+            @Override
+            public void stop() {
+
+            }
+
+            @Override
+            public DJIError checkValidity() {
+                return null;
+            }
+        };
+       // return DJISDKManager.getInstance()
+    }
 
 
 
+    boolean isRunning(){
+        return false;
+    }
 
 }
