31460e0dab8b581f932ecd51dd90d40cc3b03fe1
longhuarst <841105197@qq.com>
2019-01-01 23:34:28 +0800

    增加 MissionAction action;

--- app/src/main/java/cyfly/com/dji/cypilot3/MyMission/MyMissionAction.java
+++ app/src/main/java/cyfly/com/dji/cypilot3/MyMission/MyMissionAction.java
@@ -1,4 +1,14 @@
 package cyfly.com.dji.cypilot3.MyMission;
 
+import dji.sdk.mission.timeline.actions.MissionAction;
+
 public class MyMissionAction {
+
+
+
+    MissionAction action; //任务行动
+
+
+
+
 }
